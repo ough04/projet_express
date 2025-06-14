1ef6eca10b6295ecce0625873e44f7e3e15dd074
ough04 <oussemaghbara99@gmail.com>
2025-06-14 00:09:03 -0700

    Delete Backend/routes/user.routes.js

--- Backend/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/users');
-const { isAuthenticated, isAdmin } = require('../middleware/authMiddleware');
-router.get('/', isAuthenticated, userController.showProfile);
-router.post('/update', isAuthenticated, userController.updateProfile);
-router.get('/admin', isAuthenticated, isAdmin, userController.adminDashboard);
-module.exports = router;
